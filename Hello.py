38faa1f2a5f0597e339de0044e10c23078dc8d0c
shanhai <shanhai@gmail.com>
2021-06-03 16:56:34 +0700

    正则表达式过滤IP

--- Hello.py
+++ Hello.py
@@ -2,3 +2,13 @@
 # -*- coding: UTF-8 -*-
 
 print('Hello World!')
+
+import re
+
+pattern = re.compile(ur'((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})(\.((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})){3}')
+str = u'当前 IP：35.241.80.59  来自于：中国 香港   cloud.google.com'
+m = pattern.search(str)
+print(m.group(1))
+print(m.group(2))
+print(m.group(3))
+print(m.group(4))
